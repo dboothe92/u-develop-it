9f5b1175f6f047262b4b2e86bf3018ec51f9a8c8
Dan B <dbraunby@hotmail.com>
2021-04-06 12:25:27 -0600

    initial query

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT|| 3001;
 const app = express();
 
+//Middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+//Connect to db
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root', 
+        password: 'DanJohnBoothe1!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
+
+//Default response
 app.use((req, res) => {
     res.status(404).end();
 });
